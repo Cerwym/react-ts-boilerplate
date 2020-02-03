a29d9d7dde282dfd014956000e4dfbae1a097a9d
Peter Lockett <cerwym@googlemail.com>
2020-02-03 16:31:39 +0000

    Added first pass simple skeletom

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+interface IProps {
+
+}
+
+export class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Hello world!
+            </div>
+        )
+    }
+}
