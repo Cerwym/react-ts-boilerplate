b6fcb6ba6b77583725c9a17ef6949af7c80d5816
Peter Lockett <cerwym@googlemail.com>
2024-01-07 08:52:24 +0000

    feat: changed to vite, TS5, welcome to the new age

---
+++ src/app/app.tsx
@@ -0,0 +1,23 @@
+
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+const App = () => {
+    return (
+        <>
+            <div>
+                Hello world!
+            </div>
+            <div>
+            <a href="https://vitejs.dev" target="_blank">
+                <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+                <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+            </div>
+        </>
+    )
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import {App} from "./app";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from "./app/app";
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById('app')
+ReactDOM.createRoot(document.getElementById('app')!).render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
